d8bf907536cf84a6d0e8fa17f421b6ea60c0c7d2
Paradoxis <luke@paradoxis.nl>
2018-05-21 12:53:58 +0200

    Updated version

--- git_fingerprint/__init__.py
+++ git_fingerprint/__init__.py
@@ -2,5 +2,5 @@
 from .scanner import Scanner
 from .exceptions import GitFingerprintException
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __author__ = "Luke Paris (Paradoxis) <luke@paradoxis.nl>"
